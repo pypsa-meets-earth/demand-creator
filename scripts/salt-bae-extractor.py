4af2ac77a211075a82d551558f4a5cdea4184da6
Max Parzen <max.parzen@ed.ac.uk>
2021-12-06 23:32:29 +0100

    fix bae

--- scripts/salt-bae-extractor.py
+++ scripts/salt-bae-extractor.py
@@ -16,30 +16,28 @@ if __name__ == "__main__":
                                     number=3)
     configure_logging(snakemake)
 
-
-amount_of_pictures = snakemake.wildcards.number
-
 # setup the filename to Snakefile output name
 filenames = snakemake.output.picture
 
 # Set up the image URL
 image_url = "https://pbs.twimg.com/media/ETl5iUZXsAEv0Jc.jpg"
 
-# Open the url image, set stream to True, this will return the stream content.
-r = requests.get(image_url, stream = True)
+for name in filenames: #iterate thorugh the filenames
+    # Open the url image, set stream to True, this will return the stream content.
+    r = requests.get(image_url, stream = True)
 
-# Check if the image was retrieved successfully
-if r.status_code == 200:
-    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-    r.raw.decode_content = True
-    
-    # Open a local file with wb ( write binary ) permission.
-    
-    for name in filenames:
-            with open(name,'wb') as f:
-                shutil.copyfileobj(r.raw, f)
+    # Check if the image was retrieved successfully
+    if r.status_code == 200:
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        r.raw.decode_content = True
+        
+        # Open a local file with wb ( write binary ) permission.
+        
 
-            print('Image sucessfully Downloaded: ',name)
+        with open(name,'wb') as f:
+            shutil.copyfileobj(r.raw, f)
 
-else:
-    print('Image Couldn\'t be retreived')
+        print('Image sucessfully Downloaded: ',name)
+
+    else:
+        print('Image Couldn\'t be retreived')
